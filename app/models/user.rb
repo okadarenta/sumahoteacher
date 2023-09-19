f9e787fd8aaf9ecbe5266f6ae12b2d386443732d
okadarenta <marin111vv@gmail.com>
2023-09-19 16:04:59 +0900

    ユーザーバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :kanji_first_name, presence: true,  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+         validates :kanji_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+         validates :katakana_first_name, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :katakana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :password, format:  { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
+         validates :age, presence: true, numericality: { only_integer: true }
 end
